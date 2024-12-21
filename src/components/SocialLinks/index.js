a1ca537423abb0fee5e2111dc18a1b486d22d443
lettussandwich <csharman@student.unimelb.edu.au>
2024-12-21 13:44:37 +1100

    add portfolio link

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -5,23 +5,30 @@ import goodreadsLogo from '../../img/icons8-goodreads.svg';
 
 function SocialLinks() {
   return(
-    <div className='externalLinks'>
-      <div className='externalLink'>
-        <a href="https://www.linkedin.com/in/callum-sharman-366b532ab/" target="_blank" rel="noopener noreferrer">
-          <img className='logo' src={linkedInLogo} alt="LinkedIn Logo"/>
+    <>
+      <div className='portfolioLink'>
+        <a className='portfolioLinkInner' href="https://callumsharman.github.io/portfolio/" target="_blank" rel="noopener noreferrer">
+          PORTFOLIO
         </a>
       </div>
-      <div className='externalLink'>
-        <a href="https://github.com/callumSharman" target="_blank" rel="noopener noreferrer">
-          <img className='logo' src={gitHubLogo} alt="GitHub Logo"/>
-        </a>
+      <div className='externalLinks'>
+        <div className='externalLink'>
+          <a href="https://www.linkedin.com/in/callum-sharman-366b532ab/" target="_blank" rel="noopener noreferrer">
+            <img className='logo' src={linkedInLogo} alt="LinkedIn Logo"/>
+          </a>
+        </div>
+        <div className='externalLink'>
+          <a href="https://github.com/callumSharman" target="_blank" rel="noopener noreferrer">
+            <img className='logo' src={gitHubLogo} alt="GitHub Logo"/>
+          </a>
+        </div>
+        <div className='externalLink'>
+          <a href="https://www.goodreads.com/user/show/165214915-callum-sharman" target="_blank" rel="noopener noreferrer">
+            <img className='logo' src={goodreadsLogo} alt="Goodreads Logo"/>
+          </a>
+        </div>
       </div>
-      <div className='externalLink'>
-        <a href="https://www.goodreads.com/user/show/165214915-callum-sharman" target="_blank" rel="noopener noreferrer">
-          <img className='logo' src={goodreadsLogo} alt="Goodreads Logo"/>
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
 
