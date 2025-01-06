821da19b9786c8876c1741d76b040aac7f7a422a
lettussandwich <csharman@student.unimelb.edu.au>
2025-01-06 11:51:59 +1100

    begun gameloop

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -1,12 +1,20 @@
 import './index.css'
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function GameBoard({numRows, numCols}){
 
   const [cellsArr, setCells] = useState(Array(numRows*numCols).fill(0));
   const gridRef = useRef(null);
+  const gameLoopIntervalRef = useRef(null);
   const minCellWidth = 20; // minimum width of a cell
   const [cellWidth, setCellWidth] = useState(minCellWidth);
+
+  const [gameRunning, setGameRunning] = useState(false);
+
+  // swaps the play or pause state
+  const playPause = () => {
+    setGameRunning(!gameRunning);
+  }
   
   // fill the grid with white cells
   const emptyGrid = () => {
@@ -44,17 +52,35 @@ function GameBoard({numRows, numCols}){
     switchCellState(cellId);
   }
 
-  // 1 is alive, 0 is dead
+  // 1 is alive, 0 is dead - alive is black, dead is white
   const cells = cellsArr.map((isAlive, index) => {
     return <div className='cell' id={`cell:${index}`} onClick={handleCellClick}
               style={{width: cellWidth, height: cellWidth, backgroundColor: isAlive ? 'black' : 'white'}}></div>
   });
+
+  // update loop (game loop)
+  const update = () => {
+    randomiseGrid();
+  }
+
+  useEffect(() => {
+    if(gameRunning){
+      console.log(`gameRunning: ${gameRunning}`);
+      gameLoopIntervalRef.current = setInterval(update, 100);
+    } else {
+      clearInterval(gameLoopIntervalRef.current);
+    }
+
+
+    return () => clearInterval(gameLoopIntervalRef.current);
+  }, [gameRunning]);
 
   return(
     <>
       <button onClick={() => {emptyGrid()}}>Empty</button>
       <button onClick={() => {fillGrid()}}>Fill</button>
       <button onClick={() => {randomiseGrid()}}>Randomise</button>
+      <button onClick={() => {playPause()}}>{gameRunning ? 'Play' : 'Pause'}</button>
       <div className='grid' id='gameGrid' ref={gridRef} style={{gridTemplateColumns: `repeat(${numCols}, ${cellWidth}px)`,
                                     gridTemplateRows: `repeat(${numRows}, ${cellWidth}px)`,
       }}>
